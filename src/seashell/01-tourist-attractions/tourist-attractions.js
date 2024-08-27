7c44da92e201521deab27761e3eb71a0e16459d6
Igal Bronshtein <igal_br2@walla.co.il>
2024-08-28 00:05:29 +0300

    Update tourist-attractions.js

--- src/seashell/01-tourist-attractions/tourist-attractions.js
+++ src/seashell/01-tourist-attractions/tourist-attractions.js
@@ -1,1 +1,35 @@
+const attractionsText = `
+*********************************************
+Rijksmuseum
 
+
+
+
+
+*********************************************
+`
+
+const youtubeLinks = [
+    'https://www.youtube.com/watch?v=AOqdCNjq-x0',
+
+];
+
+function printAtractions(){
+    // remove asteriks, blank lines and extract the attractions
+    console.log('\n       Tourist Attractions 🌎\n');
+    console.log('🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫🟫\n');
+
+    let attractions = attractionsText.replace(/\*/g, '').trim().split('\n').filter(String);
+
+    attractions.forEach((place,index)=>{
+    
+        console.log(`🔹  ${place}\n`);
+ 
+        if (index < youtubeLinks.length){
+            console.log(`🔸  ${youtubeLinks[index]}\n`);
+        }
+    });
+    
+}
+
+printAtractions();
