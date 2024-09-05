42a53a652fd83ecdc296d7249962329bbd463dda
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-09-05 23:12:24 +0200

    Update cruise-ships.py

--- src/seashell/02-cruise-ships/cruise-ships.py
+++ src/seashell/02-cruise-ships/cruise-ships.py
@@ -1,11 +1,32 @@
-
-ship = {
-    
-        "name": "Icon of the seas",
-        "gross_tonnage": 248.663,
-        "net_tonnage": 307.895,
+ships = [
+    {
+        "name": "Icon of the Seas",
+        "gross_tonnage": 248663,
+        "length": 364.75,
+        
+    },
+    {
+        "name": "Utopia of the Seas",
+        "gross_tonnage": 236473,
+        "length": 361.12
+    },
+    {
+        "name": "Wonder of the Seas",
+        "gross_tonnage": 235600,
+        "length": 362.04
+    },
+    {
+        "name": "Symphony of the Seas",
+        "gross_tonnage": 228081,
+        "length": 361.01
+    },
+    {
+        "name": "Harmony of the Seas",
+        "gross_tonnage": 226963,
+        "length": 362.12
     }
-    
+]
 
 
-print(ship)
+
+print(ships)
